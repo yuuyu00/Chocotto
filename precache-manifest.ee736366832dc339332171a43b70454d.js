fb757923ee9f615153ee3bc48e09a27bd02ae2c7
yuuyu00 <my219813@gmail.com>
2019-08-02 09:40:14 +0900

    Updates

---
+++ precache-manifest.ee736366832dc339332171a43b70454d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a6820c394beb7c0ed7e2890275852f9",
+    "url": "/Choco/index.html"
+  },
+  {
+    "revision": "8a0f6aec76ba1e3b6360",
+    "url": "/Choco/static/js/2.7c608476.chunk.js"
+  },
+  {
+    "revision": "4f9a1df5771b8ec3a3a8",
+    "url": "/Choco/static/js/main.49da6a69.chunk.js"
+  },
+  {
+    "revision": "fead54f05613a94c3018",
+    "url": "/Choco/static/js/runtime~main.23ca6cf2.js"
+  }
+]);
